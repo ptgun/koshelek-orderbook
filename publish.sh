31b2535986dbec352f66a67e7ff2e438c694eee5
Pavel Tiguntsev <ptiguntsev@gmail.com>
2024-06-03 15:59:17 +0300

    add publish to github.io-pages command

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
-git add . && git commit -m "$1" && git push
-git -C ./dist add . && git -C ./dist commit -m "$1" && git -C ./dist push
+yarn build
+cd ./dist
+git add . && git -C  commit -m "update" && git push
